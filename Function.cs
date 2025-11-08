54e1980add614b224b3c69512844ae2d8e6f6ad7
Nishant patel <patel.nishant21@gmail.com>
2025-11-08 21:35:23 +0530

    Commited

--- Function.cs
+++ Function.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -12,21 +14,30 @@ namespace MyAWSLambda
 {
     public class Function
     {
-        
+
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(User input, ILambdaContext context)
+        /// For Amazon DynamoDB commented
+        //public string FunctionHandler(User input, ILambdaContext context)
+        //{
+        //    return input.Name?.ToUpper();
+        //}
+        public async Task<User> FunctionHandler(Guid Id, ILambdaContext context)
         {
-            return input.Name?.ToUpper();
+            var dynomoDBcontext = new DynamoDBContext(new AmazonDynamoDBClient());
+            var user= await dynomoDBcontext.LoadAsync<User>(Id);
+            return user;
         }
     }
+    [DynamoDBTable("User")]
     public class User
     {
-        public int Id { get; set; }
+        [DynamoDBHashKey] // <-- Required
+        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 
